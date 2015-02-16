11cdb11f7a9178d31c4fb4b523aebc86ba1dc3c9
Masashi Ogawa <ogawam@vivit.co.jp>
2015-02-16 22:38:13 +0900

    Twitterログイン

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,4 +257,5 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.omniauth :twitter, 'mZylQgPHk998q10UjQk3s43KH', '3kPbyDfNWIbWqhMQ7x8w4BaPZUf05jPUzNsRNm6joOJqnoVONF'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   #   root :to => "devise/sessions#new"
   # end
 
-  devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout'}
+  devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout'},
+             controllers: {omniauth_callbacks: 'omniauth_callbacks'}
 
   get 'home/index'
 
