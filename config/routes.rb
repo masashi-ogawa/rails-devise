8171b5c78d2c43f54e7c197c6f99b48732489d6e
Masashi Ogawa <ogawam@vivit.co.jp>
2015-02-16 19:37:29 +0900

    devise ログイン画面をroot pathに設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    root :to => "devise/sessions#new"
+  end
+
   devise_for :users
   get 'home/index'
 
@@ -8,7 +12,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  # root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
